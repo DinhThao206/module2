b52b625a8901f1664bf5829c5ef6b0cffa81d7ef
Dinh Thao <thaonguyen242006@gmail.com>
2025-11-12 15:18:19 +0700

    r

---
+++ src/Rectangle/Rectangle.java
@@ -0,0 +1,26 @@
+package Rectangle;
+
+public class Rectangle {
+    double height;
+    double width;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getArena() {
+        return this.height * this.width;
+    }
+
+    public double getPerimeter() {
+        return (this.height + this.width) * 2;
+    }
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
