453fca2c389c4379e2f68db07a2c302cb2e9cc3a
Dinh Thao <thaonguyen242006@gmail.com>
2025-11-27 13:57:43 +0700

    bubbleSort

---
+++ src/bubbleSort.java
@@ -0,0 +1,22 @@
+public class bubbleSort {
+    public static int[] list = {5, 1, 4, 2, 8, 37, 38, 39, 3, 7, 92};
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
