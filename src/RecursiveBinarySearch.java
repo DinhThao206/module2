5aaaf5f85b9d63c320df28b4110a4414cd5573e1
Dinh Thao <thaonguyen242006@gmail.com>
2025-11-27 09:54:42 +0700

    RecursiveBinarySearch

---
+++ src/RecursiveBinarySearch.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RecursiveBinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.println("Mảng sau khi sắp xếp: " + Arrays.toString(array));
+        System.out.print("Nhập giá trị cần tìm: ");
+        int value = scanner.nextInt();
+        int result = binarySearch(array, 0, array.length - 1, value);
+        if (result == -1) {
+            System.out.println("Không tìm thấy giá trị " + value + " trong mảng.");
+        } else {
+            System.out.println("Tìm thấy giá trị " + value + " tại vị trí index = " + result);
+        }
+    }
+    public static int binarySearch(int[] array, int left, int right, int value) {
+        if (left > right) {
+            return -1;
+        }
+        int middle = (left + right) / 2;
+        if (array[middle] == value) {
+            return middle;
+        }
+        if (value > array[middle]) {
+            return binarySearch(array, middle + 1, right, value);
+        }
+        return binarySearch(array, left, middle - 1, value);
+    }
+}
+
