413eddb33c9ee8732f9a3a0db3693d8aaa853b7e
Dinh Thao <thaonguyen242006@gmail.com>
2025-11-27 16:27:27 +0700

    SelectionSort

---
+++ src/SelectionSort.java
@@ -0,0 +1,25 @@
+public class SelectionSort {
+    static double[] list = {5.6, 4.5, 3.2, 7.8, 1.2, 9.0, 6.4};
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            double currentMin = list[i];
+            int currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length;i++)
+            System.out.print(list[i] + " ");
+    }
+}
